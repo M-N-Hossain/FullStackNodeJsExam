5b7565538c7917ba5791f79f81fbe367d4d8817f
nhnahid1122 <mdxx0356@stud.kea.dk>
2023-06-01 03:14:22 +0200

    implement the friend requests table and mange the query in backend and access it from frontend

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -6,20 +6,30 @@ const router = Router();
 
 router.get("/users", (req, res) => {
   const token = req.cookies.token;
-  // the sub query excludes the receiver_id from user table
-  const query = "SELECT user_id, name FROM users WHERE user_id != ? AND user_id NOT IN ( SELECT receiver_id, sender_id FROM friend_requests )";
-  // where sender_id = ?
+  const query = `
+  SELECT user_id, name
+  FROM users u
+  LEFT JOIN friend_requests fr1 ON (fr1.sender_id = ? AND fr1.receiver_id = u.user_id)
+  LEFT JOIN friend_requests fr2 ON (fr2.sender_id = u.user_id AND fr2.receiver_id = ?)
+  WHERE u.user_id != ?
+      AND fr1.request_id IS NULL
+      AND fr2.request_id IS NULL;
+`;
   Jwt.verify(token, process.env.SECRET_KEY, (err, decode) => {
     if (err) {
       return res.send({ Error: "Token does not matched" });
     } else {
-      dbConnection.query(query, [decode.user_id], (err, result) => {
-        if (err) {
-          return res.status(500).send({ Error: err });
-        } else {
-          res.send({ users: result });
+      dbConnection.query(
+        query,
+        [decode.user_id, decode.user_id, decode.user_id],
+        (err, result) => {
+          if (err) {
+            return res.status(500).send({ Error: err });
+          } else {
+            res.send({ users: result });
+          }
         }
-      });
+      );
     }
   });
 });
